17782ce30c247b889e9e4f67de7b9e01284d1796
gurugecl <gurugecl@gmail.com>
2015-12-25 23:21:34 -0600

    Update and rename mapIII.js to navmap.js

---
+++ navmap.js
@@ -0,0 +1,226 @@
+body {
+  position: relative;
+  background-color: #F9F9F9;
+  padding: 0;
+  margin: 0;
+}
+
+.tripadvisor {
+  margin-top:100px;
+  margin-left: 80px;
+}
+
+.nav a {
+  color: #5a5a5a;
+  font-size: 11px;
+  font-weight: bold;
+  padding: 14px 10px;
+  text-transform: uppercase;
+}
+
+.nav li {
+  display: inline;
+}
+
+.jumbotron {
+  background-image:url('http://paragonsrilanka.com/tl_files/sri_lanka/history.jpg');
+  height: 500px;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.jumbotron .container {
+  position: relative;
+  top:100px;
+}
+
+.jumbotron h1 {
+  color: #fff;
+  font-size: 48px;  
+  font-family: 'Shift', sans-serif;
+  font-weight: bold;
+}
+
+.jumbotron p {
+  font-size: 20px;
+  color: #fff;
+}
+
+.container .menu-link {
+  color: #fff;
+  font: bold;
+}
+
+.learn-more {
+  background-color: #f7f7f7;
+}
+
+.menu {
+  background: #202024 url('http://stock-images.antiqueprints.com/images/sm0052-Ceylon(d9764).jpg') repeat left top;
+  left: -285px;  /* start off behind the scenes */
+  height: 100%;
+  position: fixed;
+  width: 285px;
+  top: 0px;
+  z-index: 401;
+}
+
+.menu ul {
+  border-top: 1px solid #636366;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu li {
+  border-bottom: 1px solid #636366;
+  font-family: 'Open Sans', sans-serif;
+  line-height: 45px;
+  padding-bottom: 3px;
+  padding-left: 20px;
+  padding-top: 3px;
+}
+
+.menu a {
+  color: black;
+  font-size: 15px;
+  text-decoration: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+
+.icon-close {
+  cursor: pointer;
+  padding-left: 10px;
+  padding-top: 10px;
+}
+
+.icon-menu {
+  color: #fff;
+  cursor: pointer;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  padding-bottom: 25px;
+  padding-left: 25px;
+  padding-top: 25px;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+.icon-menu i {
+  margin-right: 5px;
+}
+
+
+.forecast {
+  display: inline;
+}
+
+
+#weather, #weather2, #weather3{
+  margin-top: 75px;
+}
+
+.col-md-7, .col-md-5 {
+  margin-top: 75px;
+  font-family: 'Shift', sans-serif;
+  font-weight: bold;
+}
+
+.col-md-7 {
+  left: 80px;
+}
+
+#googleMap, #googleMap2 {
+  border-style: solid;
+    border-width: 3px;
+}
+
+/* weather forecast */
+
+.weather {
+  width: 425px;
+  margin: 0px auto;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.weather h2 {
+  margin: 0 0 8px;
+  color: #fff;
+  font-size: 150px;
+  font-weight: 300;
+  text-align: center;
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);
+
+}
+
+.weather ul {
+  margin: 0;
+  padding: 0;
+}
+
+.weather li {
+  background: #fff;
+  background: rgba(255,255,255,0.90);
+  padding: 20px;
+  display: inline-block;
+  border-radius: 5px;
+}
+
+.weather p {
+  color:white;
+}
+
+.weather .currently {
+  margin: 0 20px;
+}
+
+#weather {
+  background: #1192d3 url(http://i.ytimg.com/vi/4o1TFIIciAg/maxresdefault.jpg) no-repeat bottom right;
+  background-size: 700px, 800px, 600px, 800px;
+}
+
+#weather2 {
+  background: #1192d3 url(http://www.srilankatour.travel/wp-content/uploads/2015/01/Leisure-Creations-Kandy-Big.jpg) no-repeat bottom right;
+  background-size: 700px, 800px, 600px, 800px;
+}
+
+#weather3 {
+  background: #1192d3 url(http://www.remotelands.com/images/citys/130509008.jpg) no-repeat bottom right;
+  background-size: 700px, 800px, 600px, 800px;
+}
+
+#navigation {
+position: fixed;
+top: 0;
+width: 100%;
+color: #ffffff;
+height: 72px;
+text-align: center;
+padding-top: 15px;
+/* Adds shadow to the bottom of the bar */
+-webkit-box-shadow: 0px 0px 8px 0px #000000;
+-moz-box-shadow: 0px 0px 8px 0px #000000;
+box-shadow: 0px 0px 8px 0px #000000;
+/* Adds the transparent background */
+background-color: rgba(1, 1, 1, 0.8);
+color: rgba(1, 1, 1, 0.8);
+z-index:400;
+}
+
+#navigation a {
+font-size: 14px;
+padding-left: 15px;
+padding-right: 15px;
+color: white;
+text-decoration: none;
+z-index:400;
+font-family: 'Shift', sans-serif;
+font-weight: bold;
+}
+
+#navigation a:hover {
+color: grey;
+} 
+
